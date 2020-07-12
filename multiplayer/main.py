aed3611fdc26671e5dfd3a5101cf2d0f783c8fe9
hoduchieu01 <hoduchieu01@gmail.com>
2020-07-12 12:48:22 +0700

    update multiplayer'

---
+++ multiplayer/main.py
@@ -0,0 +1,12 @@
+from userInterface import *
+
+def main():
+    master = Tk()
+    master.title("Snake and Ladder")
+    master.geometry("850x600")
+    img = PhotoImage( file = "lenna.gif")
+
+    x = Display(master,img)
+    master.mainloop()
+
+main()
